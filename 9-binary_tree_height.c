8bda2d5a2ae5ea7db523b017f18e4a5426a125fb
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-01 17:35:31 +0100

    fix: define is_leaf function within file

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,26 @@
 #include "binary_trees.h"
 #include <stdlib.h>
+
+/**
+ * is_leaf - checks if a node is a leaf node
+ * @node: pointer to node to check
+ *
+ * Return: 1 if node is a leaf otherwise 0
+ */
+int is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (node->left)
+		return (0);
+
+	if (node->right)
+		return (0);
+
+	return (1);
+}
+
 /**
  * binary_tree_height - gets height of binary tree
  * @tree: pointer to root node
@@ -17,7 +38,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	/* Height of leaf is 0 */
-	if (binary_tree_is_leaf(tree))
+	if (is_leaf(tree))
 		return (0);
 
 	height_left = binary_tree_height(tree->left);
