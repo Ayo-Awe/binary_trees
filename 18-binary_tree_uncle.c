24a847f2d658c489b1a97bbf0898fcc05c02f020
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-02 14:41:04 +0100

    feat: implement function to find uncle node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_sibling - searches for sibling node
+ * node_sibling - searches for sibling node
  * @node: pointer to node
  *
  * Return: pointer to sibling node or NULL
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *node_sibling(binary_tree_t *node)
 {
 	binary_tree_t *parent;
 
@@ -22,4 +22,19 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	/* Return left node, current node is right child of parent */
 	return (parent->left);
+}
+
+/**
+ * binary_tree_uncle - searches for uncle node
+ * @node: pointer to node
+ *
+ * Return: pointer to sibling node or NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+
+	/* Uncle node is sibling node of parent */
+	return (node_sibling(node->parent));
 }
